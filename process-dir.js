fb657d7a6feaafdb6208b7f8005bc600c5ebe8e5
Andrey Sitnik <andrey@sitnik.ru>
2021-08-12 14:34:10 +0200

    Remove unnecessary code

--- process-dir.js
+++ process-dir.js
@@ -180,9 +180,6 @@ async function replacePackage(dir, file, files, envTargets) {
       } else if (files.includes(i.replace(/\.js$/, '.browser.js'))) {
         pkg.exports[path].browser = path + '/index.browser.js'
       }
-      if (files.includes(i.replace(/\.js$/, '.browser.js'))) {
-        pkg.browser[path + '/index.cjs'] = path + '/index.browser.cjs'
-      }
       pkg.exports[path].require = path + '/index.cjs'
       pkg.exports[path].import = path + '/index.js'
       pkg.exports[path].default = path + '/index.js'
